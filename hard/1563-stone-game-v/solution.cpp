f0471e8547150a67312f2f1eb7a26aab8a5c822a
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-03 17:28:17 +0530

    ✅ Stone Game V | Hard | C++ | Runtime: 443 ms (52.64%) | Memory: 27.5 MB | 2026-08-03

--- hard/1563-stone-game-v/solution.cpp
+++ hard/1563-stone-game-v/solution.cpp
@@ -1,32 +1,40 @@
-    int sum = 0;
-    for (int i=s;i<=e;i++)sum+=v[i];
+class Solution {
+public:
+    vector<vector<int>>dp;
+    int solve(int s, int e, vector<int> &v) {
+    if (s >= e)
+        return 0;
 
-    int L = 0;
-    int ans = 0;
-    for (int i = s; i <= e; i++) {
-        L += v[i];
-        int R = sum - L;
+    if (dp[s][e] != -1)
+        return dp[s][e];
 
-        if (L > R)
-            ans = max(ans, R + solve(i + 1, e, v));
+    int sum = 0;
+    for (int i=s;i<=e;i++)sum+=v[i];
 
-        if (L <R)
-            ans = max(ans, L + solve(s, i, v));
+    int L = 0;
+    int ans = 0;
+    for (int i = s; i <= e; i++) {
+        L += v[i];
+        int R = sum - L;
 
-        if (L == R)
-            ans = max(ans,
-                      L + max(solve(i + 1, e, v),
-                              solve(s, i, v)));
-    }
+        if (L > R)
+            ans = max(ans, R + solve(i + 1, e, v));
 
-        return dp[s][e];
+        if (L <R)
+            ans = max(ans, L + solve(s, i, v));
 
+        if (L == R)
+            ans = max(ans,
+                      L + max(solve(i + 1, e, v),
+                              solve(s, i, v)));
+    }
 
-    if (dp[s][e] != -1)
-    return dp[s][e] = ans;
-        return 0;
-    if (s >= e)
+    return dp[s][e] = ans;
 }
-    int stoneGameV(vector<int>& stoneValue) {
-        int n=stoneValue.size();
-    int solve(int s, int e, vector<int> &v) {
+    int stoneGameV(vector<int>& stoneValue) {
+        int n=stoneValue.size();
+        dp.assign(n,vector<int>(n,-1));
+        return solve(0,n-1,stoneValue);
+        
+    }
+};
