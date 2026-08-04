d0a4974ebaa8748c73970f16d47034316bc90261
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-04 07:52:06 +0530

    ✅ Find Missing Elements | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 32.1 MB | 2026-08-04

--- easy/3731-find-missing-elements/solution.cpp
+++ easy/3731-find-missing-elements/solution.cpp
@@ -1,14 +1,14 @@
-class Solution {
+class Solution {
 public:
-    vector<int> findMissingElements(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        for(int i=0;i<n-1;i++){
-            for (int x = nums[i] + 1; x < nums[i + 1]; x++) {
-        }
-        vector<int>ans;
-        return ans;
-                ans.push_back(x);
-            }
-    }
+    vector<int> findMissingElements(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        vector<int>ans;
+        for(int i=0;i<n-1;i++){
+            for (int x = nums[i] + 1; x < nums[i + 1]; x++) {
+                ans.push_back(x);
+            }
+        }
+        return ans;
+    }
 };
