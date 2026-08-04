75b392ab9b4230462aa7edf23e8816a79b652821
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-04 14:41:16 +0530

    ✅ Shortest Common Supersequence | Hard | C++ | Runtime: 0 ms | Memory: 8 MB | 2026-08-04

--- hard/1092-shortest-common-supersequence/solution.cpp
+++ hard/1092-shortest-common-supersequence/solution.cpp
@@ -1,31 +1,32 @@
-    vector<vector<bool>> vis;
-    string solve(string &s1,string &s2,int i,int j){
-        if(i==s1.size() || j==s2.size())return s1.substr(i)+s2.substr(j);
+            else{
+                if(dp[i-1][j]>dp[i][j-1]){
+                    ans.push_back(s1[i-1]);
+            }
+                j--;
+                    i--;
+                }
+                else{
+                    ans.push_back(s2[j-1]);
+                    j--;
+                }
 
-        if(vis[i][j])return dp[i][j];
+            }
+                i--;
+                ans.push_back(s1[i-1]);
+            if(s1[i-1]==s2[j-1]){
+        int i=m,j=n;
+        while(i>0 && j>0){
+        string ans="";
 
-        vis[i][j]=true;
-
-        if(s1[i]==s2[j]){
-            return dp[i][j]=string(1, s1[i])+solve(s1,s2,i+1,j+1);
         }
-        else {
-            string first=string(1,s1[i])+solve(s1,s2,i+1,j);
-            string second=string(1,s2[j])+solve(s1,s2,i,j+1);
-            if(first.size()<second.size())return dp[i][j]=first;
-            else return dp[i][j]=second;
-        }
-    }
-    string shortestCommonSupersequence(string str1, string str2) {
-        int n=str1.size();
-        int m=str2.size();
-        vis.assign(n,vector<bool>(m,false));
-        dp.assign(n,vector<string>(m,"#"));
-
-        return solve(str1,str2,0,0);
-    }
-
-};
-    vector<vector<string>>dp;
-public:
-class Solution {
+        int ans_length=n+m-dp[m][n];  //length of SuperSubSequence
+            }
+                if(s1[i-1]==s2[j-1])dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            for(int j=1;j<=n;j++){
+        dp.assign(m+1,vector<int>(n+1,0));
+        for(int i=1;i<=m;i++){
+        
+        int n=s2.size();
+        
+        int m=s1.size();
