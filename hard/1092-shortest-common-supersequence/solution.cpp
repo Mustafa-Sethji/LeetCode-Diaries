760230b50eb57636bf49eecefd29ee8162fbc2fc
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-04 14:44:06 +0530

    ✅ Shortest Common Supersequence | Hard | C++ | Runtime: 26 ms (50.31%) | Memory: 26.8 MB | 2026-08-04

--- hard/1092-shortest-common-supersequence/solution.cpp
+++ hard/1092-shortest-common-supersequence/solution.cpp
@@ -1,32 +1,51 @@
-            else{
-                if(dp[i-1][j]>dp[i][j-1]){
-                    ans.push_back(s1[i-1]);
-            }
-                j--;
-                    i--;
-                }
-                else{
-                    ans.push_back(s2[j-1]);
-                    j--;
-                }
+class Solution {
+public:
+    vector<vector<int>>dp;
+    string shortestCommonSupersequence(string s1, string s2) {
+        int m=s1.size();
+        int n=s2.size();
+        
+        
+        dp.assign(m+1,vector<int>(n+1,0));
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s1[i-1]==s2[j-1])dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        int ans_length=n+m-dp[m][n];  //length of SuperSubSequence
 
-            }
-                i--;
-                ans.push_back(s1[i-1]);
-            if(s1[i-1]==s2[j-1]){
-        int i=m,j=n;
-        while(i>0 && j>0){
-        string ans="";
+        string ans="";
+        int i=m,j=n;
+        while(i>0 && j>0){
+            if(s1[i-1]==s2[j-1]){
+                ans.push_back(s1[i-1]);
+                i--;
+                j--;
+            }
+            else{
+                if(dp[i-1][j]>dp[i][j-1]){
+                    ans.push_back(s1[i-1]);
+                    i--;
+                }
+                else{
+                    ans.push_back(s2[j-1]);
+                    j--;
+                }
 
-        }
-        int ans_length=n+m-dp[m][n];  //length of SuperSubSequence
-            }
-                if(s1[i-1]==s2[j-1])dp[i][j]=1+dp[i-1][j-1];
-                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            for(int j=1;j<=n;j++){
-        dp.assign(m+1,vector<int>(n+1,0));
-        for(int i=1;i<=m;i++){
-        
-        int n=s2.size();
-        
-        int m=s1.size();
+            }
+        }
+        while(i > 0){
+            ans.push_back(s1[i-1]);
+            i--;
+        }
+
+        while(j > 0){
+            ans.push_back(s2[j-1]);
+            j--;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+
+};
