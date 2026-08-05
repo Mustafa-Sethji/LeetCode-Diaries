5b45d8d9c3e478d31d25eb3fde58dda7da8fc654
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-05 14:20:35 +0530

    ✅ Remove Methods From Project | Medium | C++ | Runtime: 768 ms (8.62%) | Memory: 326.9 MB | 2026-08-05

--- medium/3310-remove-methods-from-project/solution.cpp
+++ medium/3310-remove-methods-from-project/solution.cpp
@@ -1,32 +1,40 @@
-
-        function<void(int)> dfs = [&](int u) {
-            vis[u] = 1;
-            for (int v : graph[u]) {
-                if (!vis[v])
-                    dfs(v);
-            }
-        };
-
-        dfs(k);
-
-        // Check if any non-suspicious method calls a suspicious one
-        for (auto &e : invocations) {
-            int u = e[0], v = e[1];
-            if (!vis[u] && vis[v]) {
-                vector<int> ans;
-                for (int i = 0; i < n; i++)
-                    ans.push_back(i);
-                return ans;
-            }
-        }
-
-        vector<int> ans;
-        vector<int> vis(n, 0);
-
-        for (auto &e : invocations)
-            graph[e[0]].push_back(e[1]);
-        vector<vector<int>> graph(n);
-
+class Solution {
 public:
-    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
-class Solution {
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<vector<int>> graph(n);
+
+        for (auto &e : invocations)
+            graph[e[0]].push_back(e[1]);
+
+        vector<int> vis(n, 0);
+
+        function<void(int)> dfs = [&](int u) {
+            vis[u] = 1;
+            for (int v : graph[u]) {
+                if (!vis[v])
+                    dfs(v);
+            }
+        };
+
+        dfs(k);
+
+        // Check if any non-suspicious method calls a suspicious one
+        for (auto &e : invocations) {
+            int u = e[0], v = e[1];
+            if (!vis[u] && vis[v]) {
+                vector<int> ans;
+                for (int i = 0; i < n; i++)
+                    ans.push_back(i);
+                return ans;
+            }
+        }
+
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (!vis[i])
+                ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
