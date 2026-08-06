725bf1b8a5c2e36af58ef40418df4a4d0b685210
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-06 18:36:16 +0530

    ✅ Rotting Oranges | Medium | C++ | Runtime: 0 ms | Memory: 8.2 MB | 2026-08-06

--- medium/0994-rotting-oranges/solution.cpp
+++ medium/0994-rotting-oranges/solution.cpp
@@ -1,32 +1,32 @@
         }
             }
-                else if(grid[i][j]==1)fresh_orange++;
         int minutes=0;
         if(fresh_orange==0)return 0;
         vector<int>d_row={-1,0,1,0};
         while(!q.empty()){
-            int r=q.front().first;
-            int c=q.front().second;
-            q.pop();
+                int r=q.front().first;
+                int c=q.front().second;
+                q.pop();
         vector<int>d_col={0,1,0,-1};
-            for(int i=0;i<4;i++){
-                int new_row=r+d_row[i];
-            } 
-                int new_col=c+d_col[i];
-                if(new_row<n && new_row>-1 && new_col<m && new_col>-1){
-                    if(grid[new_row][new_col]==1){
-                }
-                        fresh_orange--;
+                for(int i=0;i<4;i++){
+                    int new_row=r+d_row[i];
+                } 
+                    int new_col=c+d_col[i];
+                    if(new_row<n && new_row>-1 && new_col<m && new_col>-1){
+                        if(grid[new_row][new_col]==1){
                     }
-                        q.push({new_row,new_col});
-                        grid[new_row][new_col]=2;
+                            fresh_orange--;
+                        }
+                            q.push({new_row,new_col});
+                            grid[new_row][new_col]=2;
+            minutes++;           
         }
 
+            int qsize=q.size();
+            while(qsize){
+            }
+                qsize--;
         if(fresh_orange==0)return minutes;
         return -1;
     }
-                if(grid[i][j]==2)q.push({i,j});
-            minutes++;           
 };
-            for(int j=0;j<m;j++){
-        for(int i=0;i<n;i++){
