3aa392e0688370371ceab681103c1d159c7ac994
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-06 07:44:36 +0530

    ✅ Smallest Divisible Digit Product I | Easy | C++ | Runtime: 0 ms | Memory: 7.9 MB | 2026-08-06

---
+++ easy/3345-smallest-divisible-digit-product-i/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int product(int x){
+        int n=x;
+        while(n!=0){
+    }
+    int smallestNumber(int n, int t) {
+        for(int i=n;i<=100;i++){
+            int digit=n/10;
+        }
+        if(n==0)return x;
+            ans*=digit;
+        int ans=1;
+            n=n/10;
+        return ans;
+            if(prod%t==0)return i;
+        }
+    }
+        return -1;
+            int prod=product(i);
+};
