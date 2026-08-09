8a9e85eb5c35f707aa5f23ca3441b6adb69f01c7
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-09 18:02:56 +0530

    ✅ 01 Matrix | Medium | C++ | Runtime: 0 ms | Memory: 8.6 MB | 2026-08-09

---
+++ medium/0542-01-matrix/solution.cpp
@@ -0,0 +1,29 @@
+        }
+            }
+                }
+                    vis[i][j]=1;
+                    dis[i][j]=0;
+
+        vector<int>d_row={-1,1,0,0};
+        vector<int>d_col={0,0,-1,1};
+
+        while(!q.empty()){
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int distance=q.front().second;
+            for(int i=0;i<4;i++){
+                int new_row=r+d_row[i];
+                int new_col=r+d_col[i];
+                if(new_row<n && new_row>=0 && new_col<m && new_col>=0 && !vis[new_row][new_col]){
+                    dis[new_row][new_col]=distance+1;
+                    q.push({{i,j},0});
+                if(mat[i][j]==0){
+                    vis[new_row][new_col]=1;
+                    q.push({{new_row,new_col},distance+1});
+                }
+            }
+        }
+        return dis;
+    }
+            q.pop();
+};
