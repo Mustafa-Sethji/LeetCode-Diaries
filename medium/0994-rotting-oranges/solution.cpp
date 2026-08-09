5f8fe69014f92ebb8f7dd8e3ea4db20dee3366d6
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-09 13:49:05 +0530

    ✅ Rotting Oranges | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 16.7 MB | 2026-08-09

--- medium/0994-rotting-oranges/solution.cpp
+++ medium/0994-rotting-oranges/solution.cpp
@@ -1,31 +1,42 @@
-        }
-            }
-        int minutes=0;
-        if(fresh_orange==0)return 0;
-        vector<int>d_row={-1,0,1,0};
-        while(!q.empty() && fresh_orange>0){
-                int r=q.front().first;
-                int c=q.front().second;
-                q.pop();
-        vector<int>d_col={0,1,0,-1};
-                for(int i=0;i<4;i++){
-                    int new_row=r+d_row[i];
-                } 
-                    int new_col=c+d_col[i];
-                    if(new_row<n && new_row>-1 && new_col<m && new_col>-1){
-                        if(grid[new_row][new_col]==1){
-                    }
-                            fresh_orange--;
-                        }
-                            q.push({new_row,new_col});
-                            grid[new_row][new_col]=2;
-            minutes++;           
-        }
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int fresh_orange=0;
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<int,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2)q.push({i,j});
+                else if(grid[i][j]==1)fresh_orange++;
+            }
+        }
+        if(fresh_orange==0)return 0;
+        int minutes=0;
+        vector<int>d_row={-1,0,1,0};
+        vector<int>d_col={0,1,0,-1};
+        while(!q.empty() && fresh_orange>0){
+            int qsize=q.size();
+            while(qsize--){
+                int r=q.front().first;
+                int c=q.front().second;
+                q.pop();
+                for(int i=0;i<4;i++){
+                    int new_row=r+d_row[i];
+                    int new_col=c+d_col[i];
+                    if(new_row<n && new_row>-1 && new_col<m && new_col>-1){
+                        if(grid[new_row][new_col]==1){
+                            fresh_orange--;
+                            q.push({new_row,new_col});
+                            grid[new_row][new_col]=2;
+                        }
+                    }
+                } 
+            }
+            minutes++;           
+        }
 
-            int qsize=q.size();
-            while(qsize--){
-            }
-        if(fresh_orange==0)return minutes;
-        return -1;
-    }
+        if(fresh_orange==0)return minutes;
+        return -1;
+    }
 };
