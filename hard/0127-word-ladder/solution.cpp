877a20edd35c8090bbb310bc95db833a35be2f31
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-09 19:20:46 +0530

    ✅ Word Ladder | Hard | C++ | Runtime: 0 ms | Memory: 8.5 MB | 2026-08-09

---
+++ hard/0127-word-ladder/solution.cpp
@@ -0,0 +1,29 @@
+        q.push({beginWord,1});
+        st.erase(beginWord);
+
+        while(!q.empty()){
+            string word=q.front().first;
+            int level=q.front().second;
+            q.pop();
+            if(word==endWord)return level;
+            for(int i=0;i<word.size();i++){
+                char original=word[i];
+                for(char ch='a';ch<='z';ch++){
+                    word[i]=ch;
+                    if(st.find(word)!=st.end()){
+                        q.push({word,level+1});
+                        st.erase(word);
+                    }
+                }
+                word[i]=original;
+            }
+        }
+
+        return 0;
+    }
+
+        queue<pair<string,int>>q;
+        
+        set<string>st(wordList.begin(),wordList.end());
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+    // }
