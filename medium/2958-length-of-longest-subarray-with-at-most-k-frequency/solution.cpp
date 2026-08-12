57f317dad9efb55594d67538f8b57d3004c8bdb9
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-12 08:01:11 +0530

    ✅ Length of Longest Subarray With at Most K Frequency | Medium | C++ | Runtime: 76 ms (21.91%) | Memory: 149.3 MB | 2026-08-12

--- medium/2958-length-of-longest-subarray-with-at-most-k-frequency/solution.cpp
+++ medium/2958-length-of-longest-subarray-with-at-most-k-frequency/solution.cpp
@@ -1,29 +1,29 @@
-class Solution {
+class Solution {
 public:
-    int maxSubarrayLength(vector<int>& nums, int k) {
-        int n=nums.size();
-        unordered_map<int,int>hash;
-        for(int i=0;i<n;i++){
-            if(hash.find(nums[i])==hash.end()){ 
-        }
-        int ans=0;
-        int j=0;
-                hash[nums[i]]++;
-            }
-            else{
-                if(hash[nums[i]]<k){
-            }
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        int n=nums.size();
+        unordered_map<int,int>hash;
+        int ans=0;
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(hash.find(nums[i])==hash.end()){ 
+                hash[nums[i]]++;
+            }
+            else{
+                if(hash[nums[i]]<k){
+                    hash[nums[i]]++;
+                }
+                else{
+                    while(hash[nums[i]]==k){
+                        hash[nums[j]]--;
+                        j++;
+                    }
+                    hash[nums[i]]++;
+                }
+            }
+            ans=max(ans,i-j+1);
 
-                    hash[nums[i]]++;
-                }
-                else{
-                    while(hash[nums[i]]==k){
-                }
-                        hash[nums[j]]--;
-                    }
-    }
-                        j++;
-            ans=max(ans,i-j+1);
-        return ans;
-                    hash[nums[i]]++;
+        }
+        return ans;
+    }
 };
