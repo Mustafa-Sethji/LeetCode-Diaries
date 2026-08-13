617b420ccd5c16631e64db4d777155cb9682fa9b
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-13 17:52:05 +0530

    ✅ Course Schedule | Medium | C++ | Runtime: 8 ms (34.50%) | Memory: 20 MB | 2026-08-13

--- medium/0207-course-schedule/solution.cpp
+++ medium/0207-course-schedule/solution.cpp
@@ -1,29 +1,32 @@
-    unordered_map<int,vector<int>>adj;
-    bool isCycleDFS(int node){
-        vis[node]=true;
-        inRec[node]=true;
-        for(int x:adj[node]){
-            if(!vis[x] && isCycleDFS(x))return true;
-            else if(vis[x] && inRec[x])return true; 
-        }
-        inRec[node]=false;
-        return false;
-    }
-    bool canFinish(int numCourses, vector<vector<int>>& prerequi) {
-        int n=prerequi.size();
-        for(int i=0;i<n;i++){
-            adj[prerequi[i][1]].emplace_back(prerequi[i][0]);
-        }
-        vis.assign(numCourses,false);
-        inRec.assign(numCourses,false);
-        for(int i=0;i<numCourses;i++){
-            if(!vis[i] && isCycleDFS(i))return false;
-        }
-        return true;
+class Solution {
+public:
+    vector<bool>vis;
+    vector<bool>inRec;
+    unordered_map<int,vector<int>>adj;
+    bool isCycleDFS(int node){
+        vis[node]=true;
+        inRec[node]=true;
+        for(int x:adj[node]){
+            if(!vis[x] && isCycleDFS(x))return true;
+            else if(vis[x] && inRec[x])return true; 
+        }
+        inRec[node]=false;
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequi) {
+        int n=prerequi.size();
+        for(int i=0;i<n;i++){
+            adj[prerequi[i][1]].emplace_back(prerequi[i][0]);
+        }
+        vis.assign(numCourses,false);
+        inRec.assign(numCourses,false);
+        for(int i=0;i<numCourses;i++){
+            if(!vis[i] && isCycleDFS(i))return false;
+        }
+        return true;
 
-    }
+    }
 };
-
 
 
 
