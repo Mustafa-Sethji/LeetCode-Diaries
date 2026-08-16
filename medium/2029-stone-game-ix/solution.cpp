f0580828b05b4b279b65da19beb68b75558c75e5
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-16 09:51:19 +0530

    ✅ Stone Game IX | Medium | C++ | Runtime: 8 ms (30.20%) | Memory: 131.3 MB | 2026-08-16

--- medium/2029-stone-game-ix/solution.cpp
+++ medium/2029-stone-game-ix/solution.cpp
@@ -1,19 +1,19 @@
-class Solution {
+class Solution {
 public:
-    bool stoneGameIX(vector<int>& stones) {
-        int cnt[3] = {0};
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {0};
 
-        // Count remainders
-        for (int x : stones) {
-            cnt[x % 3]++;
-        }
+        // Count remainders
+        for (int x : stones) {
+            cnt[x % 3]++;
+        }
 
-        // Even number of remainder-0 stones
-        if (cnt[0] % 2 == 0) {
-            return cnt[1] > 0 && cnt[2] > 0;
-        }
+        // Even number of remainder-0 stones
+        if (cnt[0] % 2 == 0) {
+            return cnt[1] > 0 && cnt[2] > 0;
+        }
 
-        // Odd number of remainder-0 stones
-        return abs(cnt[1] - cnt[2]) > 2;
-    }
+        // Odd number of remainder-0 stones
+        return abs(cnt[1] - cnt[2]) > 2;
+    }
 };
