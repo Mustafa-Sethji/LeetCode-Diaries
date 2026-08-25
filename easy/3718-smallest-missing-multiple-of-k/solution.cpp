fbfa71b435a99694cbf2e926545e49758225be3e
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-25 07:37:13 +0530

    ✅ Smallest Missing Multiple of K | Easy | C++ | Runtime: 0 ms | Memory: 8.2 MB | 2026-08-25

---
+++ easy/3718-smallest-missing-multiple-of-k/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        vector<int>in_nums(101,0);
+        for(int i=0;i<nums.size();i++){
+            in_nums[nums[i]/k]=1;
+        }
+        for(int i=1;i<in_nums.size();i++){
+            if(in_nums[i]!=1)return i;
+        }
+    }
+        return 1;
+};
