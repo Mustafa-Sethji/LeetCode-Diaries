971c3c71ffb34b2c8a2955fdbf9327a370fb9ce7
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-28 23:31:22 +0530

    ✅ Number of Matching Subsequences | Medium | C++ | Runtime: 9 ms | Memory: 10.6 MB | 2026-08-28

--- medium/0792-number-of-matching-subsequences/solution.cpp
+++ medium/0792-number-of-matching-subsequences/solution.cpp
@@ -1,32 +1,42 @@
-        for(int i=0;i<s.size();i++){
-            sort(hash[s[i]].begin(),hash[s[i]].end());
-        }
-        int count=0;
-        int n=words.size();
-    int numMatchingSubseq(string s, vector<string>& words) {
-    }
-            hash[s[i]].push_back(i);
-        }
-        for(int i=0;i<s.size();i++){
-
-
-
-        }
-        return true;
-            else return false;
-                for(int x:hash[sub[i]]){
-            }
-                    if(x>prev){
-                }
-                        prev=x;
-                    }
-                        break;
-            if(hash.find(sub[i])!=hash.end()){
-        int prev=-1;
-        for(int i=0;i<sub.size();i++){
-    bool solve(string &s,string &sub){
-    unordered_map<char,vector<int>>hash;
+class Solution {
 public:
-                bool found=false;
-                        found=true;
-                if(!found) return false;
+    unordered_map<char,vector<int>>hash;
+    bool solve(string &s,string &sub){
+        int prev=-1;
+        for(int i=0;i<sub.size();i++){
+            if(hash.find(sub[i])!=hash.end()){
+                bool found=false;
+                for(int x:hash[sub[i]]){
+                    if(x>prev){
+                        prev=x;
+                        found=true;
+                        break;
+                    }
+                }
+                if(!found) return false;
+            }
+            else return false;
+        }
+        return true;
+    }
+    int numMatchingSubseq(string s, vector<string>& words) {
+        int n=words.size();
+        int count=0;
+
+        for(int i=0;i<s.size();i++){
+            hash[s[i]].push_back(i);
+        }
+
+        for(int i=0;i<s.size();i++){
+            sort(hash[s[i]].begin(),hash[s[i]].end());
+        }
+
+        for(int i=0;i<n;i++){
+            if(solve(s,words[i])){
+                count++;
+                cout<<words[i]<<"\n";
+            }
+        }
+        return count;
+    }
+};
