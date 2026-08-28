3030eec9383115aad7e062b7be1f77945c9f1007
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-28 23:08:08 +0530

    ✅ Number of Matching Subsequences | Medium | C++ | Runtime: 0 ms | Memory: 8.5 MB | 2026-08-28

---
+++ medium/0792-number-of-matching-subsequences/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+        for(int i=0;i<n;i++){
+            if(solve(s,words[i]))count++;
+    unordered_map<char,vector<int>>hash;
+        for(int i=0;i<s.size();i++){
+            sort(hash[s[i]].begin(),hash[s[i]].end());
+        }
+        int count=0;
+        int n=words.size();
+    int numMatchingSubseq(string s, vector<string>& words) {
+    }
+        for(int i=0;i<sub.size();i++){
+    bool solve(string &s,string &sub){
+            hash[s[i]].push_back(i);
+        }
+        for(int i=0;i<s.size();i++){
+
+
+
+        int prev=-1;
+            if(hash.find(sub[i])!=hash.end()){
+        }
+        return true;
+            else return false;
+                for(int x:hash[sub[i]]){
+            }
+                    if(x>prev){
+                }
+                        prev=x;
+                    }
+                        break;
