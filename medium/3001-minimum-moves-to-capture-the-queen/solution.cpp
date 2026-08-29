eda2a4452a8e9f9a08af00edb8d4c398c42930d9
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-29 10:41:14 +0530

    ✅ Minimum Moves to Capture The Queen | Medium | C++ | Runtime: 0 ms | Memory: 7.9 MB | 2026-08-29

--- medium/3001-minimum-moves-to-capture-the-queen/solution.cpp
+++ medium/3001-minimum-moves-to-capture-the-queen/solution.cpp
@@ -3,7 +3,15 @@ public:
     int minMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f) {
 
         //  for rook (a,b)
-        if(a==e || b==f) return 1;
+        if(a==e || b==f ) {
+            if(a==c){
+                if(!(a<c<e || e<c<a))return 1;
+            }
+            else if(b=d){
+                if(!(b<d<f || f<d<b))return 1;
+            }
+            else return 1;
+        }
 
         //bishop(c,d)
         int i=c,j=d;
