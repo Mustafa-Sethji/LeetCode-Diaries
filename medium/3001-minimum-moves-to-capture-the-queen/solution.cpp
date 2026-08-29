8a98ebfbde31afc86723ec256e5a5b81f508d3b8
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-29 10:33:32 +0530

    ✅ Minimum Moves to Capture The Queen | Medium | C++ | Runtime: 0 ms | Memory: 7.8 MB | 2026-08-29

--- medium/3001-minimum-moves-to-capture-the-queen/solution.cpp
+++ medium/3001-minimum-moves-to-capture-the-queen/solution.cpp
@@ -1,29 +1,35 @@
+class Solution {
+public:
+    int minMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f) {
 
-        //bishop(c,d)
-        int i=c,j=d;
-        while(i>0 && j>0){
-            if(i==e && j==f) return 1;
-        }
-            i--;
-            j--;
-        i=c,j=d;
-        while(i>0 && j<=8){
-            if(i==e && j==f) return 1;
-            i--;
-            j++;
-        }
-        i=c,j=d;
-        while(i<=8 && j>0){
-            if(i==e && j==f) return 1;
-            i++;
-            j--;
-        }
-        i=c,j=d;
-        while(i<=8 && j<=8){
-            if(i==e && j==f) return 1;
-            i++;
-            j++;
-        }
-    }      
-        return 2;
+        //  for rook (a,b)
+        if(a==e || b==f) return 1;
+
+        //bishop(c,d)
+        int i=c,j=d;
+        while(i>0 && j>0){
+            if(i==e && j==f) return 1;
+            i--;
+            j--;
+        }
+        i=c,j=d;
+        while(i>0 && j<=8){
+            if(i==e && j==f) return 1;
+            i--;
+            j++;
+        }
+        i=c,j=d;
+        while(i<=8 && j>0){
+            if(i==e && j==f) return 1;
+            i++;
+            j--;
+        }
+        i=c,j=d;
+        while(i<=8 && j<=8){
+            if(i==e && j==f) return 1;
+            i++;
+            j++;
+        }
+        return 2;
+    }      
 };
