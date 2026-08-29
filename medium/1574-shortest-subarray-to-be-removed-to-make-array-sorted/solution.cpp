69d15149de416f68de94928f16fb0b922ea95131
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-29 09:27:34 +0530

    ✅ Shortest Subarray to be Removed to Make Array Sorted | Medium | C++ | Runtime: 0 ms | Memory: 8.4 MB | 2026-08-29

--- medium/1574-shortest-subarray-to-be-removed-to-make-array-sorted/solution.cpp
+++ medium/1574-shortest-subarray-to-be-removed-to-make-array-sorted/solution.cpp
@@ -12,16 +12,18 @@ public:
         int result=n;
 
         //step-2 start finding correct i and j and find deleted elemnt  j-i-1
-        while(){
+        while(i<j && ( i==0 || arr[i]>=arr[i-1])){ //increasing order of element from i
 
             //arr[j]>=arr[i]
             while(j<n && arr[i]>arr[j]){
                 j++;
             }
 
-            we have found correct j element such that arr[i]<=arr[j]
+            // we have found correct j element such that arr[i]<=arr[j]
 
-
+            result =min(result,j-i-1);
+            i++;
         }
+        return result;
     }
 };
