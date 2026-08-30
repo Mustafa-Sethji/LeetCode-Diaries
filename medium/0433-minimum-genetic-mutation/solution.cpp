d46ad5b0c22987aec3ee631ecefac05ca47136e4
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-30 22:08:54 +0530

    ✅ Minimum Genetic Mutation | Medium | C++ | Runtime: 0 ms | Memory: 8.5 MB | 2026-08-30

---
+++ medium/0433-minimum-genetic-mutation/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minMutation(string startGene, string endGene, vector<string>& bank) {
+        unordered_set<string>st(bank.begin(),bank.end());
+        queue<pair<string,int>>q;
+        q.push({startGene,0});
+        if(st.find(startGene)!=st.end())st.erase(startGene);
+        while(!q.empty()){
+        }
+            string word=q.front().first;
+            int level=q.front().second;
+            q.pop();
+            if(word==endGene)return level;
+            for(int i=0;i<word.size();i++){
+                char original=word[i];
+            }
+                word[i]=original;
+                for(char ch='a';ch<='z';ch++){
+                    word[i]=ch;
+                }
+                    if(ch==original)continue;
+                    if(st.find(word)!=st.end()){
+                        q.push({word,level+1});
+                    }
+    }
+                        st.erase(word);
+        return -1;
+};
