e10e02607e672946eac43a7076811e6b59bb18d0
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-30 22:11:48 +0530

    ✅ Minimum Genetic Mutation | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 8.9 MB | 2026-08-30

--- medium/0433-minimum-genetic-mutation/solution.cpp
+++ medium/0433-minimum-genetic-mutation/solution.cpp
@@ -1,28 +1,28 @@
-class Solution {
+class Solution {
 public:
-    int minMutation(string startGene, string endGene, vector<string>& bank) {
-        unordered_set<string>st(bank.begin(),bank.end());
-        queue<pair<string,int>>q;
-        q.push({startGene,0});
-        if(st.find(startGene)!=st.end())st.erase(startGene);
-        while(!q.empty()){
-        }
-            string word=q.front().first;
-            int level=q.front().second;
-            q.pop();
-            if(word==endGene)return level;
-            for(int i=0;i<word.size();i++){
-                char original=word[i];
-            }
-                word[i]=original;
-                for(char ch='A';ch<='Z';ch++){
-                    word[i]=ch;
-                }
-                    if(ch==original)continue;
-                    if(st.find(word)!=st.end()){
-                        q.push({word,level+1});
-                    }
-    }
-                        st.erase(word);
-        return -1;
+    int minMutation(string startGene, string endGene, vector<string>& bank) {
+        unordered_set<string>st(bank.begin(),bank.end());
+        queue<pair<string,int>>q;
+        q.push({startGene,0});
+        if(st.find(startGene)!=st.end())st.erase(startGene);
+        while(!q.empty()){
+            string word=q.front().first;
+            int level=q.front().second;
+            q.pop();
+            if(word==endGene)return level;
+            for(int i=0;i<word.size();i++){
+                char original=word[i];
+                for(char ch='A';ch<='Z';ch++){
+                    if(ch==original)continue;
+                    word[i]=ch;
+                    if(st.find(word)!=st.end()){
+                        q.push({word,level+1});
+                        st.erase(word);
+                    }
+                }
+                word[i]=original;
+            }
+        }
+        return -1;
+    }
 };
