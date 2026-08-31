853ee8c97a830d6c3f478e5b28a3fdac4a053131
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-31 12:54:51 +0530

    ✅ Find the Minimum and Maximum Number of Nodes Between Critical Points | Medium | C++ | Runtime: 0 ms | Memory: 8.2 MB | 2026-08-31

---
+++ medium/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/solution.cpp
@@ -0,0 +1,32 @@
+        }
+            }
+                    if(temp->val>prev && temp->val>temp->next->val)maxima.push_back(i);
+                }
+                    else if(temp->val<prev && temp->val<temp->next->val)minima.push_back(i);
+            i++;
+            temp=temp->next;
+        //for maxdistance
+        int mini=INT_MAX;
+        for(int x:minima)mini=max(maxi,x);
+        if(minima.size()+maxima.size()<2)return {-1,-1};
+        for(int x:maxima)mini=max(maxi,x);
+        int maxdis=maxi-mini;
+        for(int x:minima)mini=min(mini,x);
+        for(int x:maxima)mini=min(mini,x);
+        int maxi=-1;
+
+        int secMax=INT_MIN;
+        for(int x:minima){
+            if(secMax>x && x<maxi)secMax=x;
+        }
+            if(secMax>x && x<maxi)secMax=x;
+        }
+        for(int x:maxima){
+        int mindis=maxi-secMax;
+
+
+        //for mindistance
+
+        return {mindis,maxdis};
+    }
+};
