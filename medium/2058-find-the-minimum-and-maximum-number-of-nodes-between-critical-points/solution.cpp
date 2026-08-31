3fb19b1197421483b2589f636fcb86dac8cf95df
Mustafa Sethji <sethjimustafa@gmail.com>
2026-08-31 12:56:53 +0530

    ✅ Find the Minimum and Maximum Number of Nodes Between Critical Points | Medium | C++ | Runtime: 0 ms | Memory: 8.3 MB | 2026-08-31

--- medium/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/solution.cpp
+++ medium/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/solution.cpp
@@ -1,15 +1,11 @@
         }
-            }
-                    if(temp->val>prev && temp->val>temp->next->val)maxima.push_back(i);
-                }
-                    else if(temp->val<prev && temp->val<temp->next->val)minima.push_back(i);
             i++;
             prev=temp->val;
         //for maxdistance
         int mini=INT_MAX;
-        for(int x:minima)mini=max(maxi,x);
+        for(int x:minima)maxi=max(maxi,x);
         if(minima.size()+maxima.size()<2)return {-1,-1};
-        for(int x:maxima)mini=max(maxi,x);
+        for(int x:maxima)maxi=max(maxi,x);
         int maxdis=maxi-mini;
         for(int x:minima)mini=min(mini,x);
         for(int x:maxima)mini=min(mini,x);
@@ -17,16 +13,18 @@
 
         int secMax=INT_MIN;
         for(int x:minima){
-            if(secMax>x && x<maxi)secMax=x;
-            if(secMax>x && x<maxi)secMax=x;
+            if(secMax<x && x<maxi)secMax=x;
         }
-        for(int x:maxima){
 
         //for mindistance
-                if(temp->next!=NULL){
-            if(temp!=head){
-        while(temp!=NULL){
-        int prev=head->val;
-        int i=1;
             temp=temp->next;
+        for(int x:maxima){
+            if(secMax<x && x<maxi)secMax=x;
         }
+
+        int mindis=maxi-secMax;
+
+        return {mindis,maxdis};
+    }
+};
+
