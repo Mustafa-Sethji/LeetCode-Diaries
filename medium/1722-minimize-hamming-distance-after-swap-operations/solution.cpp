79cfddbc636f0eef4ee4c3e5ed495ac44a41a817
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-01 15:21:22 +0530

    ✅ Minimize Hamming Distance After Swap Operations | Medium | C++ | Runtime: 0 ms | Memory: 8.8 MB | 2026-09-01

---
+++ medium/1722-minimize-hamming-distance-after-swap-operations/solution.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int n;
+    int m;
+    vector<int>vis;
+    void dfs(unordered_map<int,vector<int>> &graph,int node,vector<int>&idx){
+        vis[node]=m;
+        idx.push_back(node);
+        for(int &x:graph[node]){
+            if(!vis[x])dfs(graph,x,idx);
+        }
+        return;
+    }
+    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+        n=s.size();
+        unordered_map<int,vector<int>>graph;
+        vis.assign(n,0);
+        for(auto &v:pairs){
+            graph[v[0]].push_back(v[1]);
+            graph[v[1]].push_back(v[0]);
+        }
+        m=1;
+        vector<char>ans(n); 
+
+        for(int i=0;i<n;i++)ans[i]=s[i];
+
+        for(int i=0;i<n;i++){
+            vector<int>idx;
+            if(!vis[i]) dfs(graph,i,idx);
+            else continue;
+ 
+            vector<char>character;
+            for(int x:idx)character.push_back(s[x]);
+
+            sort(idx.begin(),idx.end());
+            sort(character.begin(),character.end());
+            
+            for(int k=0;k<idx.size();k++){
+                ans[idx[k]]=character[k];
+            }
+            m++;
+        }
+
+        string a=""; 
+        for(char ch:ans)a+=ch; 
+        return a;
+
+    }
+};
