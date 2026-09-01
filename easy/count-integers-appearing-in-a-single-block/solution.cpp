594ef642dd3e3ea7246e0b1949b5181767a1edc6
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-02 00:13:20 +0530

    ✅ Q1. Count Integers Appearing in a Single Block | Easy | C++ | Runtime: 0 ms | Memory: 8.1 MB | 2026-09-01

--- easy/count-integers-appearing-in-a-single-block/solution.cpp
+++ easy/count-integers-appearing-in-a-single-block/solution.cpp
@@ -1,1 +1,18 @@
-1 <= nums.length <= 100
+class Solution {
+public:
+    int countSpecialIntegers(vector<int>& nums) {
+        int prev=-1;
+        int n=nums.size();
+        int ans=0;
+        if(n==1) return 0;
+        for(int i=1;i<n;i++){
+            if(nums[i]==nums[i-1]){
+                if(prev==-1 || nums[i]!=prev){
+                    ans++;
+                    prev=nums[i];
+                }
+            }
+        }
+        return ans;
+    }
+};
