ee49ed596c284ea90da9ef97bf1fce093c0ba7d2
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-01 11:42:01 +0530

    ✅ Smallest String With Swaps | Medium | C++ | Runtime: 0 ms | Memory: 8.4 MB | 2026-09-01

--- medium/1202-smallest-string-with-swaps/solution.cpp
+++ medium/1202-smallest-string-with-swaps/solution.cpp
@@ -1,32 +1,36 @@
-        int i=0;
-        int n=pairs.size();
-        while(i<n){
-            int j=i;
-            set<int>st;
-            while( j<n && ( st.empty() || st.find(pairs[j][0])!=st.end() || st.find(pairs[j][1])!=st.end() ) ){
-                st.insert(pairs[j][0]);
-                st.insert(pairs[j][1]);
-                j++;
-            }
-            vector<int>indices;
-            for(int x : st){
-        }
-        vector<char>ans(n_ch);
-        int n_ch=s.size();
-            for(int k=0;k<indices.size();k++){
-                ans[indices[k]]=character[k];
-            }
-            vector<char>character;
-                indices.push_back(x);
-            }
-                character.push_back(s[x]);
-            sort(character.begin(),character.end());
+class Solution {
+public:
+    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+        sort(pairs.begin(),pairs.end());
+        int i=0;
+        int n=pairs.size();
+        int n_ch=s.size();
+        vector<char>ans(n_ch);
+        while(i<n){
+            int j=i;
+            set<int>st;
+            while( j<n && ( st.empty() || st.find(pairs[j][0])!=st.end() || st.find(pairs[j][1])!=st.end() ) ){
+                st.insert(pairs[j][0]);
+                st.insert(pairs[j][1]);
+                j++;
+            }
+            vector<int>indices;
+            vector<char>character;
+            for(int x : st){
+                indices.push_back(x);
+                character.push_back(s[x]);
+            }
 
+            sort(character.begin(),character.end());
 
-        string a="";
-        for(char ch:ans)a+=ch;
-            i=j;
-        return a;
+            for(int k=0;k<indices.size();k++){
+                ans[indices[k]]=character[k];
+            }
+            i=j;
+        }
+        string a="";
+        for(char ch:ans)a+=ch;
+        return a;
 
-    }
+    }
 };
