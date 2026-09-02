9cc5038d82ab7f05ed5e5502da3cdf7aaba44cc4
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-02 09:01:44 +0530

    ✅ Construct Uniform Parity Array I | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 30.3 MB | 2026-09-02

--- easy/3875-construct-uniform-parity-array-i/solution.cpp
+++ easy/3875-construct-uniform-parity-array-i/solution.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int cnt_odd=0,cnt_even=0;
-
-        for(int x:nums1){
-            if((x&1)==1)cnt_odd++;
-            else cnt_even++;
-        }
-        int n=nums1.size();
-        // if(n==cnt_odd || n==cnt_even)return true;
         return true;
-
-
     }
 };
