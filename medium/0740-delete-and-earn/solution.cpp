d1ccc861961b14b8ab506e9dfd6a225542ccd19b
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-03 22:54:36 +0530

    ✅ Delete and Earn | Medium | C++ | Runtime: 0 ms | Memory: 8.2 MB | 2026-09-03

---
+++ medium/0740-delete-and-earn/solution.cpp
@@ -0,0 +1,33 @@
+    int deleteAndEarn(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        n=nums.size();
+        for(int i=0;i<n;i++){
+            if(mp.find(nums[i])==mp.end()){
+        }
+                arr.push_back(nums[i]);
+            }
+                mp[nums[i]]++;
+        dp.assign(n,-1);
+        return solve(0);
+    int solve(int i){
+        
+    }
+        n=arr.size();
+        if(i>=n) return 0;
+    int n;
+        if(dp[i]!=-1) return dp[i];
+
+        int take=mp[arr[i]]*arr[i];
+        if(i+1<n && arr[i]+1==arr[i+1]) take+=solve(i+2);
+        else take+=solve(i+1);
+        
+        int skip=solve(i+1);
+
+        return dp[i]=max(take,skip);
+
+    }
+    vector<int>dp;
+    vector<int>arr;
+    unordered_map<int,int>mp;
+public:
+};
