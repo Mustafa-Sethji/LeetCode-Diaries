8b0fe6ce45c05c35f600f1df6d051c9cbfedbe6d
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-03 20:17:01 +0530

    ✅ Minimum Limit of Balls in a Bag | Medium | C++ | Runtime: 0 ms | Memory: 8.2 MB | 2026-09-03

--- medium/1760-minimum-limit-of-balls-in-a-bag/solution.cpp
+++ medium/1760-minimum-limit-of-balls-in-a-bag/solution.cpp
@@ -1,31 +1,31 @@
-class Solution {
+class Solution {
 public:
-    bool solve(vector<int>&nums,int &op,int mid){
-        int k=op;
-        for(int x:nums){
-            if(x>mid){
-                while(x>mid){
-                    int p1=x-mid;
-                    int p2=mid;
-                    x=max(p1,p2);
-                    k--;
-                    if(k<0) return false;
-                }
-            }
-        }
-        return true;
-    }
-    int minimumSize(vector<int>& nums, int maxOperations) {
-        int high=*max_element(nums.begin(),nums.end());
-        int low=1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(solve(nums,maxOperations,mid)){
-                high=mid-1;
-            }
-            else low=mid+1;
-        }
-        return low;
+    bool solve(vector<int>&nums,int &op,int mid){
+        int k=op;
+        for(int x:nums){
+            if(x>mid){
+                while(x>mid){
+                    int p1=x-mid;
+                    int p2=mid;
+                    x=max(p1,p2);
+                    k--;
+                    if(k<0) return false;
+                }
+            }
+        }
+        return true;
+    }
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int high=*max_element(nums.begin(),nums.end());
+        int low=1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(solve(nums,maxOperations,mid)){
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return low;
 
-    }
+    }
 };
