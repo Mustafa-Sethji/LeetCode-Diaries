a35e9e14a48528115fbbbd2f5a0eecc350c354bb
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-04 11:55:22 +0530

    ✅ Maximal Square | Medium | C++ | Runtime: 19 ms (23.77%) | Memory: 31.2 MB | 2026-09-04

--- medium/0221-maximal-square/solution.cpp
+++ medium/0221-maximal-square/solution.cpp
@@ -7,23 +7,23 @@ public:
 
         if(dp[i][j]!=-1)return dp[i][j];
 
-        // if(matrix[i][j] == '0')return dp[i][j] = 0;
+        if(matrix[i][j] == '0')return dp[i][j] = 0;
 
         int right=solve(i,j+1,matrix);
         int below=solve(i+1,j+1,matrix);
         int left=solve(i+1,j,matrix);
 
-        return dp[i][j]=(matrix[i][j]-'0')+min(right,min(below,left));
+        return dp[i][j]=1+min(right,min(below,left));
     }
     int maximalSquare(vector<vector<char>>& matrix) {
         n=matrix.size();
         m=matrix[0].size();
         dp.assign(n,vector<int>(m,-1));
-        solve(0,0,matrix);
+        //solve(0,0,matrix);
         int maxside=INT_MIN;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                maxside=max(maxside,dp[i][j]);
+                maxside=max(maxside,solve(i,j,matrix));
             }
         }
         return maxside*maxside;
