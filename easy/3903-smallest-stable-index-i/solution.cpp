74da2543b1603fceef458f21ecd98eababe51a8f
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-04 08:51:33 +0530

    ✅ Smallest Stable Index I | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 31.2 MB | 2026-09-04

--- easy/3903-smallest-stable-index-i/solution.cpp
+++ easy/3903-smallest-stable-index-i/solution.cpp
@@ -1,21 +1,21 @@
-class Solution {
+class Solution {
 public:
-    int firstStableIndex(vector<int>& nums, int k) {
-        int n=nums.size();
-        vector<int>temp(n);
-        for(int i=0;i<n;i++){
-            temp[i]=maxi;
-        }
-        int maxi=INT_MIN;
-            maxi=max(maxi,nums[i]);
-        for(int i=n-1;i>=0;i--){
-            mini=min(mini,nums[i]);
-        }
-        int mini=INT_MAX;
-            temp[i]-=mini;
-        for(int i=0;i<n;i++){
-            if(temp[i]<=k) return i;
-        }
-    }
-        return -1;
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int>temp(n);
+        int maxi=INT_MIN;
+        for(int i=0;i<n;i++){
+            maxi=max(maxi,nums[i]);
+            temp[i]=maxi;
+        }
+        int mini=INT_MAX;
+        for(int i=n-1;i>=0;i--){
+            mini=min(mini,nums[i]);
+            temp[i]-=mini;
+        }
+        for(int i=0;i<n;i++){
+            if(temp[i]<=k) return i;
+        }
+        return -1;
+    }
 };
