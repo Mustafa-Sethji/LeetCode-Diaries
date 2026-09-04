7e40ecd5bab27612e95086c0031c6fc093ac8dab
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-04 11:53:25 +0530

    ✅ Maximal Square | Medium | C++ | Runtime: 0 ms | Memory: 8.6 MB | 2026-09-04

--- medium/0221-maximal-square/solution.cpp
+++ medium/0221-maximal-square/solution.cpp
@@ -1,31 +1,31 @@
-class Solution {
+class Solution {
 public:
-    vector<vector<int>>dp;
-    int solve(int i,int j,vector<vector<char>>& matrix){
-    int maximalSquare(vector<vector<char>>& matrix) {
-        n=matrix.size();
-        m=matrix[0].size();
-        dp.assign(n,vector<int>(m,-1));
-        solve(0,0,matrix);
-        if(i>=n || j>=m)return 0;
-    }
-        int right=solve(i,j+1,matrix);
-        int below=solve(i+1,j+1,matrix);
-        int left=solve(i+1,j,matrix);
+    vector<vector<int>>dp;
+    int n,m;
+    int solve(int i,int j,vector<vector<char>>& matrix){
+        if(i>=n || j>=m)return 0;
 
-        return dp[i][j]=(matrix[i][j]-'0')+min(right,min(below,left));
+        if(dp[i][j]!=-1)return dp[i][j];
 
-        // if(matrix[i][j] == '0')return dp[i][j] = 0;
+        // if(matrix[i][j] == '0')return dp[i][j] = 0;
 
-        if(dp[i][j]!=-1)return dp[i][j];
+        int right=solve(i,j+1,matrix);
+        int below=solve(i+1,j+1,matrix);
+        int left=solve(i+1,j,matrix);
 
-    int n,m;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-        }
-                maxside=max(maxside,dp[i][j]);
-            }
-    }
-        int maxside=INT_MIN;
-        return maxside*maxside;
+        return dp[i][j]=(matrix[i][j]-'0')+min(right,min(below,left));
+    }
+    int maximalSquare(vector<vector<char>>& matrix) {
+        n=matrix.size();
+        m=matrix[0].size();
+        dp.assign(n,vector<int>(m,-1));
+        solve(0,0,matrix);
+        int maxside=INT_MIN;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                maxside=max(maxside,dp[i][j]);
+            }
+        }
+        return maxside*maxside;
+    }
 };
