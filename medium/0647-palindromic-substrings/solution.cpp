88bc8ad8116c31d80cc20c493cadc0a794eafcb8
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-04 12:49:02 +0530

    ✅ Palindromic Substrings | Medium | C++ | Runtime: 43 ms (22.57%) | Memory: 34.7 MB | 2026-09-04

---
+++ medium/0647-palindromic-substrings/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+
+    bool solve(int i, int j, string& s) {
+        if (i >= j)
+            return true;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (s[i] != s[j])
+            return dp[i][j] = false;
+
+        return dp[i][j] = solve(i + 1, j - 1, s);
+    }
+
+    int countSubstrings(string s) {
+        int n = s.size();
+
+        dp.assign(n, vector<int>(n, -1));
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (solve(i, j, s))
+                    ans++;
+            }
+        }
+
+        return ans;
+    }
+};
