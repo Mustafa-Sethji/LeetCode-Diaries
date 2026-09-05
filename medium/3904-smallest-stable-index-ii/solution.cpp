1f5ba678b92a4f173ae3b0693260ed3391639e46
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-05 19:34:24 +0530

    ✅ Smallest Stable Index II | Medium | C++ | Runtime: 16 ms (34.38%) | Memory: 202.5 MB | 2026-09-05

--- medium/3904-smallest-stable-index-ii/solution.cpp
+++ medium/3904-smallest-stable-index-ii/solution.cpp
@@ -1,20 +1,20 @@
-class Solution {
+class Solution {
 public:
-    int firstStableIndex(vector<int>& nums, int k) {
-        int n=nums.size();
-    
-        vector<int>mx(n,0);
-        int maxi=INT_MIN;
-        for(int i=0;i<n;i++){
-            maxi=max(maxi,nums[i]);
-        }
-            mx[i]+=maxi;
-        return -1;
-        int mini=INT_MAX;
-            mini=min(mini,nums[n-i-1]);
-            mx[n-i-1]-=mini;
-        for(int i=0;i<n;i++){
-            if(mx[i]<=k)return i;
-        }
-    }
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n=nums.size();
+    
+        vector<int>mx(n,0);
+        int maxi=INT_MIN;
+        int mini=INT_MAX;
+        for(int i=0;i<n;i++){
+            maxi=max(maxi,nums[i]);
+            mini=min(mini,nums[n-i-1]);
+            mx[i]+=maxi;
+            mx[n-i-1]-=mini;
+        }
+        for(int i=0;i<n;i++){
+            if(mx[i]<=k)return i;
+        }
+        return -1;
+    }
 };
