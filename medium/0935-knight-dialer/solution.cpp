5758f8d39f3f236fee8e913481c52fc36149d4d0
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-08 15:12:59 +0530

    ✅ Knight Dialer | Medium | C++ | Runtime: 128 ms (18.04%) | Memory: 41.3 MB | 2026-09-08

--- medium/0935-knight-dialer/solution.cpp
+++ medium/0935-knight-dialer/solution.cpp
@@ -1,31 +1,35 @@
-    int knightDialer(int n) {
-        unordered_map<int,vector<int>>dial;
-        dial[0]={4,6};
-        dial[1]={6,8};
-        dial[2]={7,9};
-        dial[3]={4,8};
-        dial[4]={0,3,9};
-        
-        dial[6]={0,1,7};
-        dial[7]={2,6};
-        dial[8]={1,3};
-        }
-        return dp[length][num]=paths;
-    }
-        int ans=0;
-        dial[9]={2,4};
-            paths+=solve(x,length-1,dial,dp);
-        int paths=0;
-        for(int x:dial[num]){
-        vector<vector<int>>dp(n,vector<int>(10,-1));
-        for(int i=0;i<10;i++){
-            ans = (ans + solve(i, n - 1, dial, dp)) % MOD;
-        }
-        return ans;
-            paths %= MOD;
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+    int solve(int num,int length,unordered_map<int,vector<int>>&dial,vector<vector<int>>&dp){
 
-    }
+        if(length==0)return 1;
+        if(dp[length][num]!=-1)return dp[length][num];
+        int paths=0;
+        for(int x:dial[num]){
+            paths+=solve(x,length-1,dial,dp);
+            paths %= MOD;
+        }
+        return dp[length][num]=paths;
+    }
+    int knightDialer(int n) {
+        unordered_map<int,vector<int>>dial;
+        dial[0]={4,6};
+        dial[1]={6,8};
+        dial[2]={7,9};
+        dial[3]={4,8};
+        dial[4]={0,3,9};
+        
+        dial[6]={0,1,7};
+        dial[7]={2,6};
+        dial[8]={1,3};
+        dial[9]={2,4};
+        int ans=0;
+        vector<vector<int>>dp(n,vector<int>(10,-1));
+        for(int i=0;i<10;i++){
+            ans = (ans + solve(i, n - 1, dial, dp)) % MOD;
+        }
+        return ans;
+
+    }
 };
-        if(dp[length][num]!=-1)return dp[length][num];
-        if(length==0)return 1;
-
