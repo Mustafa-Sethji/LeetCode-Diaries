19bd0cc2cc4d45679a90f1087b3448a3a010ece7
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-08 11:28:23 +0530

    ✅ Continuous Subarray Sum | Medium | C++ | Runtime: 0 ms | Memory: 8.2 MB | 2026-09-08

--- medium/0523-continuous-subarray-sum/solution.cpp
+++ medium/0523-continuous-subarray-sum/solution.cpp
@@ -5,7 +5,7 @@ public:
         int sum=0;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            if(mp.find(sum%k)!=mp.end())return true;
+            if(mp.find(sum%k)!=mp.end() && nums[i]!=0)return true;
             else if(sum%k==0 && sum!=0) return true;
             else mp[sum%k]=1;
         }
