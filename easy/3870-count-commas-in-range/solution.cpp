bae824754a6adb6517fc8db71d879ae671d068e3
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-08 11:17:04 +0530

    ✅ Count Commas in Range | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 8.6 MB | 2026-09-08

---
+++ easy/3870-count-commas-in-range/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        if(n<1000)return 0;
+       // else if(n>=1000 && n<10000){
+            return n-1000+1;
+        //}
+    //else if(n>=10000 && n<100000)return
+    }
+};
