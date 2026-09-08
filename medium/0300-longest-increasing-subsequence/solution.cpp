048a8c4ecb0b935365d448f8047bf6349e07cb6b
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-08 15:42:58 +0530

    ✅ Longest Increasing Subsequence | Medium | C++ | Runtime: 0 ms | Memory: 8.4 MB | 2026-09-08

--- medium/0300-longest-increasing-subsequence/solution.cpp
+++ medium/0300-longest-increasing-subsequence/solution.cpp
@@ -1,3 +1,4 @@
+       
     }
     int lengthOfLIS(vector<int>& nums) {
         n=nums.size();
@@ -12,21 +13,20 @@
         //             tabulation_dp[i]=max(tabulation_dp[i],tabulation_dp[j]+1);
         //     }
         // }
+        }
+
+        return dp[i][prev+1]=max(take,not_take);
+        if(prev==-1 || nums[i]>nums[prev]){
+            take=1+solve(i+1,i,nums);
+        int take=0;
+        int not_take=0+solve(i+1,prev,nums);
+
+        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+
     int n;
     int solve(int i,int prev,vector<int> &nums){
-        if(i>=n)return 0;
+        if(i>=n) return 0;
+
 class Solution {
 public:
     vector<vector<int>>dp;
-        // return *max_element(tabulation_dp.begin(),tabulation_dp.end());
-
-
-        if(dp[i][prev+1]!=-1)return dp[i][prev+1];
-
-        int not_take=0+solve(i+1,prev,nums);
-
-        int take=INT_MIN;
-        if(prev==-1 || nums[i]>nums[prev]) take=1+solve(i+1,i,nums);
-
-        return dp[i][prev+1]=max(take,not_take);
-
