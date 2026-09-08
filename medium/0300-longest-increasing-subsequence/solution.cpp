6c6d06fc4b672532c389fbfa74ccb95c9c25fd0e
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-08 15:37:46 +0530

    ✅ Longest Increasing Subsequence | Medium | C++ | Runtime: 0 ms | Memory: 8.5 MB | 2026-09-08

---
+++ medium/0300-longest-increasing-subsequence/solution.cpp
@@ -0,0 +1,32 @@
+       
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        n=nums.size();
+        dp.assign(n,vector<int>(n+1,-1));
+        return solve(0,-1,nums);
+
+
+        // vector<int>tabulation_dp(n,1);
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<i;j++){
+        //         if(nums[i]>nums[j])
+        //             tabulation_dp[i]=max(tabulation_dp[i],tabulation_dp[j]+1);
+        //     }
+        // }
+        if(i>=n)return 0;
+    int solve(int i,int prev,vector<int> &nums){
+    vector<vector<int>>dp;
+    int n;
+public:
+class Solution {
+        // return *max_element(tabulation_dp.begin(),tabulation_dp.end());
+
+
+        if(dp[i][prev+1]!=-1)return dp[i][prev+1];
+
+        int not_take=0+solve(i+1,prev,nums);
+        if(prev==-1 || i<n-1 && nums[i]<nums[i+1]) take=1+solve(i+1,i,nums);
+
+        int take=INT_MIN;
+
+        return dp[i][prev+1]=max(take,not_take);
