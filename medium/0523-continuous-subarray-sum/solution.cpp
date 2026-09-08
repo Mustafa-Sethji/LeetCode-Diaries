4f5c3e3cd7b85a3e2d96b2e5fa10fd37b276bbc7
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-08 11:30:54 +0530

    ✅ Continuous Subarray Sum | Medium | C++ | Runtime: 75 ms (78.33%) | Memory: 146.2 MB | 2026-09-08

--- medium/0523-continuous-subarray-sum/solution.cpp
+++ medium/0523-continuous-subarray-sum/solution.cpp
@@ -3,11 +3,18 @@ public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         unordered_map<int,int>mp;
         int sum=0;
-        for(int i=0;i<nums.size();i++){
+        int n=nums.size();
+        mp[0]=-1;
+        for(int i=0;i<n;i++){
             sum+=nums[i];
-            if(mp.find(sum%k)!=mp.end() && nums[i]!=0)return true;
-            else if(sum%k==0 && sum!=0) return true;
-            else mp[sum%k]=1;
+            int rem=sum%k;
+
+            //check if its present in map
+
+            if(mp.find(rem)!=mp.end()){
+                if(i-mp[rem]>=2) return true;
+            }
+            else mp[rem]=i;
         }
         return false;
     }
