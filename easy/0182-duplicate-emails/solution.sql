cd67b8b3b838987f9f9a528aa3814ad37cdf4f53
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-09 22:30:51 +0530

    ✅ Duplicate Emails | Easy | SQL | Runtime: 638 ms (6.46%) | Memory: 0B | 2026-09-09

--- easy/0182-duplicate-emails/solution.sql
+++ easy/0182-duplicate-emails/solution.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
-SELECT e1.name Employee
-FROM Employee e1
-INNER JOIN Employee e2
-ON e1.managerId=e2.id
-WHERE e1.salary>e2.salary
+select email Email
+FROM Person 
+GROUP BY email
+HAVING COUNT(DISTINCT id)>1
