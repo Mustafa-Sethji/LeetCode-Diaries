5322e6000d2fa27bb4041c4ae1a71edf9f274f65
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-10 00:25:32 +0530

    ✅ Range Sum Query 2D - Immutable | Medium | C++ | Runtime: 20 ms (74.96%) | Memory: 149.3 MB | 2026-09-09

--- medium/0304-range-sum-query-2d-immutable/solution.cpp
+++ medium/0304-range-sum-query-2d-immutable/solution.cpp
@@ -25,14 +25,10 @@ public:
     }
 
     int sumRegion(int row1, int col1, int row2, int col2) {
-        return prefix[row2 ][col2]
-             - prefix[row1-1][col2]
-             - prefix[row2][col1-1]
-             + prefix[row1-1][col1-1];
         int ans=prefix[row2][col2];
         if(row1>0)ans-=prefix[row1-1][col2];
         if(col1>0)ans-=prefix[row2][col1-1];
-        if(row1>0 && col1>0)ans+=prefix[row1-1][col2-1];
+        if(row1>0 && col1>0)ans+=prefix[row1-1][col1-1];
         return ans;
     }
 };
