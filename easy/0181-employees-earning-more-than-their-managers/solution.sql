5f2cb4777139275dea9afc10a38ed217fc0c299f
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-09 22:25:01 +0530

    ✅ Employees Earning More Than Their Managers | Easy | SQL | Runtime: 403 ms (66.48%) | Memory: 0B | 2026-09-09

--- easy/0181-employees-earning-more-than-their-managers/solution.sql
+++ easy/0181-employees-earning-more-than-their-managers/solution.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
-SELECT p.firstName,p.lastName,a.city,a.state        
-FROM Person p
-LEFT JOIN Address a
-ON p.personId=a.personId         
+SELECT e1.name Employee
+FROM Employee e1
+LEFT JOIN Employee e2
+ON e1.managerId=e2.id
+WHERE e1.salary>e2.salary
