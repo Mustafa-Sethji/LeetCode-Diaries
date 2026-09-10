14e2c948bb7fd1bcd4c91e9d235fe3befb79e110
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-10 10:00:28 +0530

    ✅ Generate Parentheses | Medium | C++ | Runtime: 3 ms (67.74%) | Memory: 15.5 MB | 2026-09-10

--- medium/0022-generate-parentheses/solution.cpp
+++ medium/0022-generate-parentheses/solution.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
-    typedef vector <string> vistr;
-    void generate(vistr &vi,int n,string str,int open,int close){
-        if(str.size()==2*n){
-            vi.emplace_back(str);
+    void generate(vector<string>&v,int &n,string s,int open,int close){
+        if(s.size()==2*n){
+            v.emplace_back(s);
             return;
         }
-        if(open<n)generate(vi,n,str+'(',open+1,close);
-        if(close<open)generate(vi,n,str+')',open,close+1);
+        if(open<n)generate(v,n,s+'(',open+1,close);
+        if(close<open)generate(v,n,s+')',open,close+1);
+
+        return;
     }
     vector<string> generateParenthesis(int n) {
-        vistr collect;
+        // vistr collect;
+        // string str="";
+        // generate(collect,n,str,0,0);
+        // return collect;
+        vector<string>collect;
         string str="";
         generate(collect,n,str,0,0);
         return collect;
