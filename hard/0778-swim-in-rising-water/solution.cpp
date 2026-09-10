8b1bf5ee4421f75f0d8f6595f1fb9c29b0eed845
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-11 00:30:26 +0530

    ✅ Swim in Rising Water | Hard | C++ | Runtime: 3 ms (87.64%) | Memory: 14.4 MB | 2026-09-10

--- hard/0778-swim-in-rising-water/solution.cpp
+++ hard/0778-swim-in-rising-water/solution.cpp
@@ -6,7 +6,7 @@ public:
     bool dfs(vector<vector<int>>&grid,int time,int row,int col,vector<vector<int>>&vis){
         vis[row][col]=1;
         if(row==n-1 && col==n-1)return true;
-        
+
         for(int i=0;i<4;i++){
             int nrow=row+delrow[i];
             int ncol=col+delcol[i];
@@ -18,7 +18,7 @@ public:
     }
     int swimInWater(vector<vector<int>>& grid) {
         n=grid.size();
-        int low=0,high=n*n-1;
+        int low=grid[0][0],high=n*n-1;
         int mid=0;
         while(low<=high){
             mid=low+(high-low)/2;
