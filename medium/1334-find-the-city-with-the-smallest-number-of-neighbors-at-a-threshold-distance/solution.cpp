afb85bc4f6c7348d85085d8e12b65dd334929d3a
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-11 18:54:46 +0530

    ✅ Find the City With the Smallest Number of Neighbors at a Threshold Distance | Medium | C++ | Runtime: 0 ms | Memory: 8.6 MB | 2026-09-11

--- medium/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/solution.cpp
+++ medium/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/solution.cpp
@@ -1,5 +1,3 @@
-        }
-        for(int i=0;i<n;i++) dist[i][i]=0;
         for(int via=0;via<n;via++){
             for(int i=0;i<n;i++){
         }
@@ -22,11 +20,9 @@
             int rechable=0;
             if(i==0)ans={i,rechable};
             else{
-                if(rechable<ans.second)ans={i,rechable};
-            dist[it[1]][it[0]]=it[2];
-            dist[it[0]][it[1]]=it[2];
-        for(auto it:edges){
-        vector<vector<int>>dist(n,vector<int>(n,INT_MAX));
-    int findTheCity(int n, vector<vector<int>>& edges, int dt){
-public:
-class Solution {
+                if(rechable<=ans.second)ans={i,rechable};
+            }
+        }
+        return ans.first;
+    }
+};
