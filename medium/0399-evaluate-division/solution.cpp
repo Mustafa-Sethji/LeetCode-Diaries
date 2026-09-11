906a4b10fc451e92cdc79202964a58dbb8baec48
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-11 22:28:43 +0530

    ✅ Evaluate Division | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 11.8 MB | 2026-09-11

--- medium/0399-evaluate-division/solution.cpp
+++ medium/0399-evaluate-division/solution.cpp
@@ -1,28 +1,47 @@
-        vector<vector<double>>adj(lab,vector<double>(lab,INT_MAX));
-        for(int i=0;i<lab;i++)adj[i][i]=1;
+class Solution {
+public:
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        unordered_map<int,string>label;
+        unordered_map<string,int>label_chk;
+        int lab=0;
+        for(int i=0;i<equations.size();i++){
+            if(label_chk.find(equations[i][0])==label_chk.end()){
+                label[lab]=equations[i][0];
+                label_chk[equations[i][0]]=lab;
+                lab++;
+            }
+            if(label_chk.find(equations[i][1])==label_chk.end()){
+                label[lab]=equations[i][1];
+                label_chk[equations[i][1]]=lab;
+                lab++;
+            }
+        }
 
-        for(int i=0;i<equations.size();i++){
-            adj[label_chk[equations[i][0]]][label_chk[equations[i][1]]]=values[i];
-            adj[label_chk[equations[i][1]]][label_chk[equations[i][0]]]=1/values[i];
-        }
 
-        for(int via=0;via<lab;via++){
-            for(int i=0;i<lab;i++){
-                for(int j=0;j<lab;j++){
-                    if(adj[i][via]!=INT_MAX && adj[via][j]!=INT_MAX && adj[i][j]==INT_MAX){
-                        adj[i][j]=adj[i][via]*adj[via][j];
-                    }
-                }
-            }
-        }
-        vector<double>ans;
-        for(int i=0;i<queries.size();i++){
-            if(label_chk.find(queries[i][0])==label_chk.end() || label_chk.find(queries[i][1])==label_chk.end() )ans.push_back(-1);
-            else if(adj[label_chk[queries[i][0]]][label_chk[queries[i][1]]]!=INT_MAX)ans.push_back(adj[label_chk[queries[i][0]]]
-            [label_chk[queries[i][1]]]);
-            else ans.push_back(-1);
-        }
+        vector<vector<double>>adj(lab,vector<double>(lab,INT_MAX));
+        for(int i=0;i<lab;i++)adj[i][i]=1;
 
-        return ans;
-    }
+        for(int i=0;i<equations.size();i++){
+            adj[label_chk[equations[i][0]]][label_chk[equations[i][1]]]=values[i];
+            adj[label_chk[equations[i][1]]][label_chk[equations[i][0]]]=1/values[i];
+        }
+
+        for(int via=0;via<lab;via++){
+            for(int i=0;i<lab;i++){
+                for(int j=0;j<lab;j++){
+                    if(adj[i][via]!=INT_MAX && adj[via][j]!=INT_MAX && adj[i][j]==INT_MAX){
+                        adj[i][j]=adj[i][via]*adj[via][j];
+                    }
+                }
+            }
+        }
+        vector<double>ans;
+        for(int i=0;i<queries.size();i++){
+            if(label_chk.find(queries[i][0])==label_chk.end() || label_chk.find(queries[i][1])==label_chk.end() )ans.push_back(-1);
+            else if(adj[label_chk[queries[i][0]]][label_chk[queries[i][1]]]!=INT_MAX)ans.push_back(adj[label_chk[queries[i][0]]][label_chk[queries[i][1]]]);
+            else ans.push_back(-1);
+        }
+
+        return ans;
+    }
 };
