202c762807107345708f4e83c9f35ba9b38eb821
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-11 11:39:24 +0530

    ✅ Minimum Cost to Make at Least One Valid Path in a Grid | Hard | C++ | Runtime: 3 ms | Memory: 8.8 MB | 2026-09-11

--- hard/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/solution.cpp
+++ hard/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/solution.cpp
@@ -1,30 +1,30 @@
-class Solution {
+class Solution {
 public:
-    int minCost(vector<vector<int>>& grid) {
-        m=grid.size();
-        n=grid[0].size();
-        vector<vector<bool>>vis(m,vector<bool>(n,false));
-        return dfs(0,0,grid,vis,0); //explore all paths by backtracking
+    vector<vector<int>>del={{0,1},{0,-1},{1,0},{-1,0}};
+    int m,n;
+    int dfs(int row,int col,vector<vector<int>>&grid,vector<vector<bool>>&vis,int cost){
+        if(row==m-1 && col==n-1)return cost;
+        vis[row][col]=true;
+        int min_cost=INT_MAX;
+        //now explore
+        for(int i=0;i<4;i++){
+            int nrow=row+del[i][0];
+            int ncol=col+del[i][1];
+            if(nrow<m && nrow>=0 && ncol<n && ncol>=0 && !vis[nrow][ncol]){
+                int nexcost=cost+ ((grid[row][col]-1!=i) ? 1:0 );
+                min_cost=min(min_cost,dfs(nrow,ncol,grid,vis,nexcost));
+            }
+        }
 
-    int dfs(int row,int col,vector<vector<int>>&grid,vector<vector<bool>>&vis,int cost){
-        vis[row][col]=true;
-    }
-    vector<vector<int>>del={{0,1},{0,-1},{1,0},{-1,0}};
+        vis[row][col]=false;
+        return min_cost;
 
-        if(row==m-1 && col==n-1)return cost;
-    int m,n;
-        //now explore
+    }
+    int minCost(vector<vector<int>>& grid) {
+        m=grid.size();
+        n=grid[0].size();
+        vector<vector<bool>>vis(m,vector<bool>(n,false));
+        return dfs(0,0,grid,vis,0); //explore all paths by backtracking
 
-        vis[row][col]=false;
-        return min_cost;
-        int min_cost=INT_MAX;
-        for(int i=0;i<4;i++){
-            int nrow=row+del[i][0];
-        }
-            int ncol=col+del[i][1];
-            if(nrow<m && nrow>=0 && ncol<n && ncol>=0 && !vis[nrow][ncol]){
-                int nexcost=cost+ ((grid[row][col]-1!=i) ? 1:0 );
-            }
-    }
-                min_cost=min(min_cost,dfs(nrow,ncol,grid,vis,nexcost));
+    }
 };
