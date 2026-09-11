faea8d19a2db671983174dbe6ce74729d012927f
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-11 11:55:50 +0530

    ✅ Network Delay Time | Medium | C++ | Runtime: 101 ms (11.17%) | Memory: 48.1 MB | 2026-09-11

--- medium/0743-network-delay-time/solution.cpp
+++ medium/0743-network-delay-time/solution.cpp
@@ -1,29 +1,29 @@
-class Solution {
+class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        unordered_map<int,vector<pair<int,int>>>mp;
-        for(auto it:times){
-            mp[it[0]].push_back({it[1],it[2]});
-        }
-        queue<pair<int,int>>q;
-        vector<int>dist(n+1,INT_MAX);
-        q.push({k,0});
-        while(!q.empty()){
-            int node=q.front().first;
-            int time=q.front().second;
-            q.pop();
-            for(auto it:mp[node]){
-                int curr=it.first;
-                int new_time=time+it.second;
-                if(curr<n+1 && dist[curr]>new_time){
-                    dist[curr]=new_time;
-                    q.push({curr,new_time});
-                }
-            }
-        }
-        int mx=*max_element(dist.begin(),dist.end());
-        if(mx==INT_MAX)return -1;
-        return mx;
-    }
-        dist[k]=0;
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        unordered_map<int,vector<pair<int,int>>>mp;
+        for(auto it:times){
+            mp[it[0]].push_back({it[1],it[2]});
+        }
+        queue<pair<int,int>>q;
+        vector<int>dist(n+1,INT_MAX);
+        dist[k]=0;
+        q.push({k,0});
+        while(!q.empty()){
+            int node=q.front().first;
+            int time=q.front().second;
+            q.pop();
+            for(auto it:mp[node]){
+                int curr=it.first;
+                int new_time=time+it.second;
+                if(curr<n+1 && dist[curr]>new_time){
+                    dist[curr]=new_time;
+                    q.push({curr,new_time});
+                }
+            }
+        }
+        int mx=*max_element(dist.begin()+1,dist.end());
+        if(mx==INT_MAX)return -1;
+        return mx;
+    }
 };
