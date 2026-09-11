594f4244e31c0d4744522798765dabf82ea662b8
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-11 18:58:02 +0530

    ✅ Find the City With the Smallest Number of Neighbors at a Threshold Distance | Medium | C++ | Runtime: 8 ms (92.87%) | Memory: 17.5 MB | 2026-09-11

--- medium/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/solution.cpp
+++ medium/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/solution.cpp
@@ -1,28 +1,37 @@
-        for(int via=0;via<n;via++){
-            for(int i=0;i<n;i++){
-        }
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int dt){
+        vector<vector<int>>dist(n,vector<int>(n,INT_MAX));
+        for(auto it:edges){
+            dist[it[0]][it[1]]=it[2];
+            dist[it[1]][it[0]]=it[2];
+        }
+        for(int i=0;i<n;i++) dist[i][i]=0;
 
-                for(int j=0;j<n;j++){
-            }
-                    if( dist[i][via]!=INT_MAX && 
-                }
-                        dist[via][j]!=INT_MAX &&
-                        dist[i][via]+dist[via][j]<dist[i][j] ){
-                        dist[i][j]=dist[i][via]+dist[via][j];
+        for(int via=0;via<n;via++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if( dist[i][via]!=INT_MAX && 
+                        dist[via][j]!=INT_MAX &&
+                        dist[i][via]+dist[via][j]<dist[i][j] ){
 
-                    }
+                        dist[i][j]=dist[i][via]+dist[via][j];
 
-        pair<int,int>ans={-1,-1};
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(dist[i][j]<=dt)rechable+=1;
-            }
-            int rechable=0;
-            if(i==0)ans={i,rechable};
-            else{
-                if(rechable<=ans.second)ans={i,rechable};
-            }
-        }
-        return ans.first;
-    }
+                    }
+                }
+            }
+        }
+        pair<int,int>ans={-1,-1};
+        for(int i=0;i<n;i++){
+            int rechable=0;
+            for(int j=0;j<n;j++){
+                if(dist[i][j]<=dt)rechable+=1;
+            }
+            if(i==0)ans={i,rechable};
+            else{
+                if(rechable<=ans.second)ans={i,rechable};
+            }
+        }
+        return ans.first;
+    }
 };
