8b86c22726e7167518c576beacdeade9189e5374
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-13 17:19:43 +0530

    ✅ Pairs of Songs With Total Durations Divisible by 60 | Medium | C++ | Runtime: 0 ms | Memory: 8.2 MB | 2026-09-13

---
+++ medium/1010-pairs-of-songs-with-total-durations-divisible-by-60/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numPairsDivisibleBy60(vector<int>& time) {
+        vector<int>freq(60,0);
+        int ans=0;
+        for(int i=0;i<time.size();i++){
+            int rem=time[i]%60;
+        }
+    }
+        return ans;
+            int need=(60-rem)%60;
+            ans+=freq[need];
+            freq[rem]++;
+};
