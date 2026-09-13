db131ca9287ad284b8422fcf9dec3ab7a3290b88
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-13 18:42:12 +0530

    ✅ Combination Sum | Medium | C++ | Runtime: 0 ms | Memory: 8.5 MB | 2026-09-13

--- medium/0039-combination-sum/solution.cpp
+++ medium/0039-combination-sum/solution.cpp
@@ -3,13 +3,13 @@ public:
     vector<vector<int>>ans;
     int n;
     void solve(int i,vector<int>&temp,int sum,vector<int>&nums){
-        if(i>=n)return;
+        if(i>=n )return;
         if(sum==0){
             ans.push_back(temp);
             return;
         }
         if(nums[i]>sum)return;
-        else if(nums[i]<sum){
+        else if(nums[i]<=sum){
             temp.emplace_back(nums[i]);
             solve(i,temp,sum-nums[i],nums);
             temp.pop_back();
