f11ef183119c2e44cbee9774190effceecb2c439
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-13 18:30:38 +0530

    ✅ Find the Duplicate Number | Medium | C++ | Runtime: 0 ms | Memory: 8.2 MB | 2026-09-13

--- medium/0287-find-the-duplicate-number/solution.cpp
+++ medium/0287-find-the-duplicate-number/solution.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         int slow=0;
-        return nums[p];
+        return p;
         int fast=0;
         do{
-            slow=nums[nums[slow]];
+            slow=nums[slow];
         }while(slow!=fast);
-            fast=nums[nums[fast]];
-            fast=nums[nums[fast]];
+            fast=nums[nums[fast]];;
         int p=0;
         while(p!=slow){
         }
     }
-            p=nums[nums[p]];
-            slow=nums[nums[slow]];
+            p=nums[p];
+            slow=nums[slow];
 };
