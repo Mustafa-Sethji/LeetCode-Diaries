d85936b4baa3bf81ac7dbfc31269b1986f474567
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-13 18:44:11 +0530

    ✅ Combination Sum | Medium | C++ | Runtime: 0 ms | Memory: 8.6 MB | 2026-09-13

--- medium/0039-combination-sum/solution.cpp
+++ medium/0039-combination-sum/solution.cpp
@@ -1,25 +1,25 @@
-class Solution {
+class Solution {
 public:
-    vector<vector<int>>ans;
-    int n;
-    void solve(int i,vector<int>&temp,int sum,vector<int>&nums){
-        if(i>=n )return;
-        if(sum==0){
-            ans.push_back(temp);
-            return;
-        }
-        if(nums[i]>sum)return;
-        else if(nums[i]<=sum){
-            temp.emplace_back(nums[i]);
-            solve(i,temp,sum-nums[i],nums);
-            temp.pop_back();
-        }
-        solve(i+1,temp,sum,nums);
-    }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        n=candidates.size();
-        vector<int>temp;
-        solve(0,temp,target,candidates);
-        return ans;
-    }
+    vector<vector<int>>ans;
+    int n;
+    void solve(int i,vector<int>&temp,int sum,vector<int>&nums){
+        if(i>=n )return;
+        if(sum==0){
+            ans.push_back(temp);
+            return;
+        }
+        if(nums[i]>sum)return;
+        else if(nums[i]<=sum){
+            temp.emplace_back(nums[i]);
+            solve(i,temp,sum-nums[i],nums);
+            temp.pop_back();
+        }
+        solve(i+1,temp,sum,nums);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        n=candidates.size();
+        vector<int>temp;
+        solve(0,temp,target,candidates);
+        return ans;
+    }
 };
