cb7a294557d7f5fe7574226dd76361b8c380af12
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-13 12:20:39 +0530

    ✅ Keys and Rooms | Medium | C++ | Runtime: 5 ms (5.93%) | Memory: 16.6 MB | 2026-09-13

--- medium/0841-keys-and-rooms/solution.cpp
+++ medium/0841-keys-and-rooms/solution.cpp
@@ -1,30 +1,30 @@
-class Solution {
+class Solution {
 public:
-    bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        room=rooms.size();
-    int room;
-        unordered_map<int,vector<int>>adj;
-        for(int i=0;i<room;i++){
-            for(auto &it:rooms[i]){
-        }
-                adj[i].push_back(it);
-            }
+    int room;
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        room=rooms.size();
+        unordered_map<int,vector<int>>adj;
 
-        queue<int>q;
-        vector<int>vis(room,0);
-        q.push(0);
-        vis[0]=1;
-        while(!q.empty()){
-            int node=q.front();
-        }
-            q.pop();
-            for(auto key:adj[node]){
-                if(!vis[key]){
-            }
-        for(int x: vis)if(x==0)return false;    
-        return true;
-                    q.push(key);
-                }
-    }
-                    vis[key]=1;
+        for(int i=0;i<room;i++){
+            for(auto &it:rooms[i]){
+                adj[i].push_back(it);
+            }
+        }
+        queue<int>q;
+        vector<int>vis(room,0);
+        q.push(0);
+        vis[0]=1;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(auto key:adj[node]){
+                if(!vis[key]){
+                    q.push(key);
+                    vis[key]=1;
+                }
+            }
+        }
+        for(int x: vis)if(x==0)return false;    
+        return true;
+    }
 };
