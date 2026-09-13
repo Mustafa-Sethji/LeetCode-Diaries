57808161a37f0c17e0bac6d02fa47de6fbc298b2
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-13 10:35:44 +0530

    ✅ Making A Large Island | Hard | C++ | Runtime: 23 ms | Memory: 21.8 MB | 2026-09-13

--- hard/0827-making-a-large-island/solution.cpp
+++ hard/0827-making-a-large-island/solution.cpp
@@ -1,29 +1,40 @@
-    int largestIsland(vector<vector<int>>& grid) {
-        n=grid.size();
-        int maxArea=INT_MIN;
-        for(int i=0;i<n;i++){
-    }
-            for(int j=0;j<n;j++){
-        }
-                if(grid[i][j]==1 && !visited[i][j]) maxArea=max(maxArea,dfs(i,j,grid,visited));
-            }
-        vector<vector<bool>>visited(n,vector<bool>(n,false));
+class Solution {
+public:
+    int n;
+    vector<vector<int>>directions={{0,1},{0,-1},{1,0},{-1,0}};
+    int dfs(int i,int j,vector<vector<int>>&grid,vector<vector<bool>>&vis){
+        if(i>=n || j>=n || i<0 || j<0 || vis[i][j] || grid[i][j]==0) return 0;
+        vis[i][j]=true;
+        int siz=1;
+        for(auto &dir:directions){
+            int nrow=dir[0]+i;
+            int ncol=dir[1]+j;
+            siz+=dfs(nrow,ncol,grid,vis);
+        }
+        return siz;
 
-        }
-            siz+=dfs(nrow,ncol,grid,vis);
-        return siz;
+    }
+    int largestIsland(vector<vector<int>>& grid) {
+        n=grid.size();
+        int maxArea=INT_MIN;
+        vector<vector<bool>>visited(n,vector<bool>(n,false));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1 && !visited[i][j]) maxArea=max(maxArea,dfs(i,j,grid,visited));
+            }
+        }
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==0){
-                    grid[i][j]=1;
-                    vector<vector<bool>>vis(n,vector<bool>(n,false));
-                    maxArea=max(maxArea,dfs(i,j,grid,vis));
-                    grid[i][j]=0;
-                }
-            }
-        }
-        return maxArea;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==0){
+                    grid[i][j]=1;
+                    vector<vector<bool>>vis(n,vector<bool>(n,false));
+                    maxArea=max(maxArea,dfs(i,j,grid,vis));
+                    grid[i][j]=0;
+                }
+            }
+        }
+        return maxArea;
 
-    }
+    }
 };
