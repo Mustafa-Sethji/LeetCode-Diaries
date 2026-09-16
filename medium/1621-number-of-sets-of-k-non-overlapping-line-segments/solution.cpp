e1c8bc846da959f5597f0facd0c4c2fadf79548a
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-16 14:41:18 +0530

    ✅ Number of Sets of K Non-Overlapping Line Segments | Medium | C++ | Runtime: 73 ms (42.52%) | Memory: 145.4 MB | 2026-09-16

---
+++ medium/1621-number-of-sets-of-k-non-overlapping-line-segments/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    const long long MOD = 1e9 + 7;
+    vector<vector<long long>> dp;
+
+    long long solve(int n, int k) {
+        if (k == 0 || k == n)
+            return 1;
+
+        if (k < 0 || k > n)
+            return 0;
+
+        if (dp[n][k] != -1)
+            return dp[n][k];
+
+        return dp[n][k] =
+            (solve(n - 1, k - 1) + solve(n - 1, k)) % MOD;
+    }
+
+    int numberOfSets(int n, int k) {
+        int N = n + k - 1;
+
+        dp.assign(N + 1, vector<long long>(2 * k + 1, -1));
+
+        return solve(N, 2 * k);
+    }
+};
