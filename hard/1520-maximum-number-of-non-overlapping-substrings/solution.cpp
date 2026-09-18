b7572a2437fa3b5bd382375f75f90a9415ad6a25
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-18 16:40:38 +0530

    ✅ Maximum Number of Non-Overlapping Substrings | Hard | C++ | Runtime: 4 ms (92.58%) | Memory: 23.3 MB | 2026-09-18

---
+++ hard/1520-maximum-number-of-non-overlapping-substrings/solution.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    vector<string> maxNumOfSubstrings(string s) {
+        int n = s.size();
+
+        vector<int> first(26, n);
+        vector<int> last(26, -1);
+
+        // Find first and last occurrence
+        for (int i = 0; i < n; i++) {
+            int x = s[i] - 'a';
+            first[x] = min(first[x], i);
+            last[x] = i;
+        }
+
+        vector<pair<int, int>> intervals;
+
+        // Try to construct a valid interval for each character
+        for (int c = 0; c < 26; c++) {
+
+            if (last[c] == -1)
+                continue;
+
+            int l = first[c];
+            int r = last[c];
+
+            bool valid = true;
+
+            for (int i = l; i <= r; i++) {
+
+                int x = s[i] - 'a';
+
+                // This character appeared before l
+                if (first[x] < l) {
+                    valid = false;
+                    break;
+                }
+
+                // We must include all occurrences of this character
+                r = max(r, last[x]);
+            }
+
+            if (valid) {
+                intervals.push_back({l, r});
+            }
+        }
+
+        // Sort by ending position
+        sort(intervals.begin(), intervals.end(),
+             [](auto &a, auto &b) {
+                 return a.second < b.second;
+             });
+
+        vector<string> ans;
+        int prevEnd = -1;
+
+        for (auto &[l, r] : intervals) {
+            if (l > prevEnd) {
+                ans.push_back(s.substr(l, r - l + 1));
+                prevEnd = r;
+            }
+        }
+
+        return ans;
+    }
+};
