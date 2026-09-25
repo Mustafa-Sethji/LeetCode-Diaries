c51763e09c926f25b479777e208679f4c6a4c116
Mustafa Sethji <sethjimustafa@gmail.com>
2026-09-25 15:52:25 +0530

    ✅ Binary Tree Maximum Path Sum | Hard | C++ | Runtime: 0 ms (100.00%) | Memory: 27.9 MB | 2026-09-25

---
+++ hard/0124-binary-tree-maximum-path-sum/solution.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>maxi_sum;
+    int maxi=INT_MIN;
+    int single_maxi=INT_MIN;
+    int func(TreeNode* root){
+        if(root==NULL)return 0;
+        if(root->left==NULL && root->right==NULL) {
+            maxi=max(root->val,maxi);
+            return root->val;
+        }
+        int lh=func(root->left);
+        int rh=func(root->right);
+        maxi=max(maxi,max(root->val+lh,max(root->val+rh,root->val)));
+        if(root->left!=NULL && root->right!=NULL)single_maxi=max(single_maxi,root->val+lh+rh);
+        int mossi=max(root->val+lh,max(root->val+rh,root->val));
+        return mossi;
+    }
+    int maxPathSum(TreeNode* root) {
+        int h=func(root);
+        maxi=max(single_maxi,maxi);
+        return max(maxi,h);
+    }
+};
+
+
+
+//CLASSICAL CODE
+// class Solution {
+// public:
+//     int func(TreeNode* root,int &maxi){
+//         if(root==NULL)return 0;
+//         int lh=max(0,func(root->left,maxi));
+//         int rh=max(0,func(root->right,maxi));
+//         maxi=max(maxi,root->val+lh+rh);
+//         return root->val+max(lh,rh);
+//     }
+//     int maxPathSum(TreeNode* root) {
+//         int maxi=INT_MIN;
+//         func(root,maxi);
+//         return maxi;
+//     }
+// };
